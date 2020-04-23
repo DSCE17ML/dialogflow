58325e2c7f824a54a717a879ed8f4c9045973a19
Harshitha shetty <harshithadshetty@gmail.com>
2020-04-23 13:39:41 +0530

    app

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from twilio.rest import Client
 account_sid = 'ACd6aad6cb6e31bb8fc324d95e7142c763'
 auth_token = 'a1221b507dea4800bd00c7a3d221c4e8'
@@ -8,7 +7,7 @@ client = Client(account_sid, auth_token)
 
 message = client.messages \
     .create(
-         body='',
+         body='Hi Test for Twilio.',
          from_='+19798032727',
          to='+919818225401'
      )
